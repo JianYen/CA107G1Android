10c3cbbf37db22335e52d65672465aaca410c0eb
gf941728 <gf941728@gmail.com>
2019-05-21 13:28:13 +0800

    OK

--- app/src/main/java/com/yen/CA107G1/Server/ServerURL.java
+++ app/src/main/java/com/yen/CA107G1/Server/ServerURL.java
@@ -3,33 +3,49 @@ package com.yen.CA107G1.Server;
 public class ServerURL {
 
 
-//    public static final String Shop_URL = "http://172.20.10.12:8081/CA107G1/Android/shopItem.do";
-//    public static final String Member_URL = "http://172.20.10.12:8081/CA107G1/Android/member.do";
-//    public static final String MemberPic_URL = "http://172.20.10.12:8081/CA107G1/Android/ImageLoad";
-//    public static final String RoomType_URL = "http://172.20.10.12:8081/CA107G1/Android/RoomType.do";
-//    public static final String Pet_URL = "http://172.20.10.12:8081/CA107G1/Android/PetServlet.do";
-//    public static final String HotelOrder_URL = "http://172.20.10.12:8081/CA107G1/Android/HotelOrder.do";
-//    public static final String PetImgUpLoad_URL = "http://172.20.10.12:8081/CA107G1/Android/PetImgUpLoadServlet.do";
-//    public static final String HotelRoomTypeMsg_URL = "http://172.20.10.12:8081/CA107G1/Android/HotelRoomTypeMsgServlet.do";
-//    public static final String ShopItemImg_URL = "http://172.20.10.12:8081/CA107G1";
-//    public static final String News_URL = "http://172.20.10.12:8081/CA107G1/Android/NewsServlet.do";
-//    public static final String Emp_URL = "http://172.20.10.12:8081/CA107G1/Android/EmpServlet.do";
-//    public static final String ShopItemCover_URL = "http://172.20.10.12:8081/CA107G1/Android/ShopItemCoverServlet.do";
-//    public static final String ShopOrdServlet_URL = "http://172.20.10.12:8081/CA107G1/Android/ShopOrdServlet.do";
+    public static final String Shop_URL = "http://172.20.10.12:8081/CA107G1/Android/shopItem.do";
+    public static final String Member_URL = "http://172.20.10.12:8081/CA107G1/Android/member.do";
+    public static final String MemberPic_URL = "http://172.20.10.12:8081/CA107G1/Android/ImageLoad";
+    public static final String RoomType_URL = "http://172.20.10.12:8081/CA107G1/Android/RoomType.do";
+    public static final String Pet_URL = "http://172.20.10.12:8081/CA107G1/Android/PetServlet.do";
+    public static final String HotelOrder_URL = "http://172.20.10.12:8081/CA107G1/Android/HotelOrder.do";
+    public static final String PetImgUpLoad_URL = "http://172.20.10.12:8081/CA107G1/Android/PetImgUpLoadServlet.do";
+    public static final String HotelRoomTypeMsg_URL = "http://172.20.10.12:8081/CA107G1/Android/HotelRoomTypeMsgServlet.do";
+    public static final String ShopItemImg_URL = "http://172.20.10.12:8081/CA107G1";
+    public static final String News_URL = "http://172.20.10.12:8081/CA107G1/Android/NewsServlet.do";
+    public static final String Emp_URL = "http://172.20.10.12:8081/CA107G1/Android/EmpServlet.do";
+    public static final String ShopItemCover_URL = "http://172.20.10.12:8081/CA107G1/Android/ShopItemCoverServlet.do";
+    public static final String ShopOrdServlet_URL = "http://172.20.10.12:8081/CA107G1/Android/ShopOrdServlet.do";
 
-    public static final String Shop_URL = "http://10.120.26.6:8081/CA107G1/Android/shopItem.do";
-    public static final String Member_URL = "http://10.120.26.6:8081/CA107G1/Android/member.do";
-    public static final String MemberPic_URL = "http://10.120.26.6:8081/CA107G1/Android/ImageLoad";
-    public static final String RoomType_URL = "http://10.120.26.6:8081/CA107G1/Android/RoomType.do";
-    public static final String Pet_URL = "http://10.120.26.6:8081/CA107G1/Android/PetServlet.do";
-    public static final String HotelOrder_URL = "http://10.120.26.6:8081/CA107G1/Android/HotelOrder.do";
-    public static final String PetImgUpLoad_URL = "http://10.120.26.6:8081/CA107G1/Android/PetImgUpLoadServlet.do";
-    public static final String HotelRoomTypeMsg_URL = "http://10.120.26.6:8081/CA107G1/Android/HotelRoomTypeMsgServlet.do";
-    public static final String ShopItemImg_URL = "http://10.120.26.6:8081/CA107G1";
-    public static final String News_URL = "http://10.120.26.6:8081/CA107G1/Android/NewsServlet.do";
-    public static final String Emp_URL = "http://10.120.26.6:8081/CA107G1/Android/EmpServlet.do";
-    public static final String ShopItemCover_URL = "http://10.120.26.6:8081/CA107G1/Android/ShopItemCoverServlet.do";
-    public static final String ShopOrdServlet_URL = "http://10.120.26.6:8081/CA107G1/Android/ShopOrdServlet.do";
+//    public static final String Shop_URL = "http://10.120.26.6:8081/CA107G1/Android/shopItem.do";
+//    public static final String Member_URL = "http://10.120.26.6:8081/CA107G1/Android/member.do";
+//    public static final String MemberPic_URL = "http://10.120.26.6:8081/CA107G1/Android/ImageLoad";
+//    public static final String RoomType_URL = "http://10.120.26.6:8081/CA107G1/Android/RoomType.do";
+//    public static final String Pet_URL = "http://10.120.26.6:8081/CA107G1/Android/PetServlet.do";
+//    public static final String HotelOrder_URL = "http://10.120.26.6:8081/CA107G1/Android/HotelOrder.do";
+//    public static final String PetImgUpLoad_URL = "http://10.120.26.6:8081/CA107G1/Android/PetImgUpLoadServlet.do";
+//    public static final String HotelRoomTypeMsg_URL = "http://10.120.26.6:8081/CA107G1/Android/HotelRoomTypeMsgServlet.do";
+//    public static final String ShopItemImg_URL = "http://10.120.26.6:8081/CA107G1";
+//    public static final String News_URL = "http://10.120.26.6:8081/CA107G1/Android/NewsServlet.do";
+//    public static final String Emp_URL = "http://10.120.26.6:8081/CA107G1/Android/EmpServlet.do";
+//    public static final String ShopItemCover_URL = "http://10.120.26.6:8081/CA107G1/Android/ShopItemCoverServlet.do";
+//    public static final String ShopOrdServlet_URL = "http://10.120.26.6:8081/CA107G1/Android/ShopOrdServlet.do";
+
+
+//    public static final String Shop_URL = "http://35.201.129.244:8081/CA107G1/Android/shopItem.do";
+//    public static final String Member_URL = "http://35.201.129.244:8081/CA107G1/Android/member.do";
+//    public static final String MemberPic_URL = "http://35.201.129.244:8081/CA107G1/Android/ImageLoad";
+//    public static final String RoomType_URL = "http://35.201.129.244:8081/CA107G1/Android/RoomType.do";
+//    public static final String Pet_URL = "http://35.201.129.244:8081/CA107G1/Android/PetServlet.do";
+//    public static final String HotelOrder_URL = "http://35.201.129.244:8081/CA107G1/Android/HotelOrder.do";
+//    public static final String PetImgUpLoad_URL = "http://35.201.129.244:8081/CA107G1/Android/PetImgUpLoadServlet.do";
+//    public static final String HotelRoomTypeMsg_URL = "http://35.201.129.244:8081/CA107G1/Android/HotelRoomTypeMsgServlet.do";
+//    public static final String ShopItemImg_URL = "http://35.201.129.244:8081/CA107G1";
+//    public static final String News_URL = "http://35.201.129.244:8081/CA107G1/Android/NewsServlet.do";
+//    public static final String Emp_URL = "http://35.201.129.244:8081/CA107G1/Android/EmpServlet.do";
+//    public static final String ShopItemCover_URL = "http://35.201.129.244:8081/CA107G1/Android/ShopItemCoverServlet.do";
+//    public static final String ShopOrdServlet_URL = "http://35.201.129.244:8081/CA107G1/Android/ShopOrdServlet.do";
+
 
 
 }
